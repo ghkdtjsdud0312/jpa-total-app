6ff73523b07ceec9d5a9f958b9049627d201d97f
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2023-11-24 09:57:40 +0900

    11/24 chatting

--- src/main/java/com/kh/jpatotalapp/config/WebSocketHandler.java
+++ src/main/java/com/kh/jpatotalapp/config/WebSocketHandler.java
@@ -1,30 +1,46 @@
-//package com.kh.jpatotalapp.config;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.kh.jpatotalapp.dto.ChatMessageDto;
-//import com.kh.jpatotalapp.dto.ChatRoomResDto;
-//import com.kh.jpatotalapp.service.ChatService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.socket.TextMessage;
-//import org.springframework.web.socket.WebSocketSession;
-//import org.springframework.web.socket.handler.TextWebSocketHandler;
-//
-//@RequiredArgsConstructor
-//@Slf4j
-//@Component
-//public class WebSocketHandler extends TextWebSocketHandler {
-//
-//    private final ObjectMapper objectMapper;
-//    private final ChatService chatService;
-//    @Override
-//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-//        String payload = message.getPayload();
-//        log.warn("{}", payload);
-//        ChatMessageDto chatMessage = objectMapper.readValue(payload, ChatMessageDto.class);
-//        ChatRoomResDto chatRoom = chatService.findRoomById(chatMessage.getRoomId());
-//        chatRoom.handlerActions(session, chatMessage, chatService);
-//    }
-//
-//}
+package com.kh.jpatotalapp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.jpatotalapp.dto.ChatMessageDto;
+import com.kh.jpatotalapp.dto.ChatRoomResDto;
+import com.kh.jpatotalapp.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+    private final Map<WebSocketSession, String> sessionRoomIdMap = new ConcurrentHashMap<>();
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.warn("{}", payload);
+        ChatMessageDto chatMessage = objectMapper.readValue(payload, ChatMessageDto.class);
+        ChatRoomResDto chatRoom = chatService.findRoomById(chatMessage.getRoomId());
+        // 세션과 채팅방 ID를 매핑
+        sessionRoomIdMap.put(session, chatMessage.getRoomId());
+        chatRoom.handlerActions(session, chatMessage, chatService);
+    }
+    @Override
+    // WebSocket 연결의 생명주기를 관리하는데 있어 중요한 부분을 담당, 연결 종료 직 후 호출
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        // 세션과 매핑된 채팅방 ID 가져오기
+        String roomId = sessionRoomIdMap.remove(session);
+        if (roomId != null) {
+            ChatRoomResDto chatRoom = chatService.findRoomById(roomId);
+            chatRoom.handleSessionClosed(session, chatService);
+        }
+    }
+}
--- src/main/java/com/kh/jpatotalapp/service/ChatService.java
+++ src/main/java/com/kh/jpatotalapp/service/ChatService.java
@@ -1,53 +1,63 @@
-//package com.kh.jpatotalapp.service;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.kh.jpatotalapp.dto.ChatRoomResDto;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.socket.TextMessage;
-//import org.springframework.web.socket.WebSocketSession;
-//
-//import javax.annotation.PostConstruct;
-//import java.io.IOException;
-//import java.time.LocalDateTime;
-//import java.util.*;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//@Service
-//public class ChatService {
-//    private final ObjectMapper objectMapper;
-//    private Map<String, ChatRoomResDto> chatRooms;
-//
-//    @PostConstruct // 의존성 주입 이후 초기화를 수행하는 메소드
-//    private void init() {
-//        chatRooms = new LinkedHashMap<>();
-//    }
-//    public List<ChatRoomResDto> findAllRoom() {
-//        return new ArrayList<>(chatRooms.values());
-//    }
-//    public ChatRoomResDto findRoomById(String roomId) {
-//        return chatRooms.get(roomId);
-//    }
-//
-//    // 방 개설하기
-//    public ChatRoomResDto createRoom(String name) {
-//        String randomId = UUID.randomUUID().toString();
-//        log.info("UUID : " + randomId);
-//        ChatRoomResDto chatRoom = ChatRoomResDto.builder()
-//                .roomId(randomId)
-//                .name(name)
-//                .regDate(LocalDateTime.now())
-//                .build();
-//        chatRooms.put(randomId, chatRoom);
-//        return chatRoom;
-//    }
-//    public <T> void sendMessage(WebSocketSession session, T message) {
-//        try {
-//            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
-//        } catch(IOException e) {
-//            log.error(e.getMessage(), e);
-//        }
-//    }
-//}
+package com.kh.jpatotalapp.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.jpatotalapp.dto.ChatRoomResDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+    private final ObjectMapper objectMapper;
+    private Map<String, ChatRoomResDto> chatRooms;
+
+    @PostConstruct // 의존성 주입 이후 초기화를 수행하는 메소드, 주로 초기화 로직에서 사용
+    private void init() {
+        chatRooms = new LinkedHashMap<>(); // 삽입순서를 유지하는 해시맵
+    }
+    public List<ChatRoomResDto> findAllRoom() {
+        return new ArrayList<>(chatRooms.values());
+    }
+    public ChatRoomResDto findRoomById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    // 방 개설하기
+    public ChatRoomResDto createRoom(String name) {
+        String randomId = UUID.randomUUID().toString();
+        log.info("UUID : " + randomId);
+        ChatRoomResDto chatRoom = ChatRoomResDto.builder()
+                .roomId(randomId)
+                .name(name)
+                .regDate(LocalDateTime.now())
+                .build();
+        chatRooms.put(randomId, chatRoom);
+        return chatRoom;
+    }
+    // 주어진 ID를 가진 채팅방을 chatRooms에서 제거합니다. 방에 세션이 없는 경우에만 제거합니다.
+    public void removeRoom(String roomId) {
+        ChatRoomResDto room = chatRooms.get(roomId);
+        if (room != null) {
+            if (room.isSessionEmpty()) {
+                chatRooms.remove(roomId);
+            }
+        }
+    }
+    // 특정 WebSocketSession에 메시지 전송
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+        } catch(IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
